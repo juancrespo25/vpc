9db23786e618a7aeddedf1e40c7b42758b2b231f
juancrespo25 <jcrespovirhuez@gmail.com>
2023-05-27 18:57:21 -0500

    actualizando archivos de vpc

--- main.tf
+++ main.tf
@@ -1,51 +1,34 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-}
-
-resource "aws_instance" "cloudcamp" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = var.subnet_id
-
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr
   tags = {
-    bootcamp = "devops"
+    Name = "main-2048"
   }
-
-  associate_public_ip_address = true
-  key_name = var.key_pair_name
-  user_data = file("${path.module}/userdata.sh")
 }
 
-resource "aws_security_group" "sg" {
-  name        = "allow_http"
-  description = "Allow inbound traffic"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.cidr, 8, 1)
+  tags = {
+    Name = "public-2048"
   }
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
+  tags = {
+    Name = "route-2048"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "ec2_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.cloudcamp.public_ip
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
 }
